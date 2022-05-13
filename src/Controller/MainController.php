7db3c6b8b99b8f6ca40be31a0dde82b80a8a5956
LinhLe2307 <lemylinh23071997@gmail.com>
2022-05-13 17:17:48 +0300

    Add basic layout for homepage, header and footer

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,8 +11,29 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(): Response
     {
+        $meals = [
+            'Mini pizzas' => 'Delectable and bite-sized, mini pizzas are a favorite of all age groups.',
+            'Homemade French Fries' => 'Surprise your guests with the best gourmet french fries they’ve ever tasted.',
+            'Stuffed Mushrooms' => 'A kitchen classic, this is the perfect choice if you’ve vegans in your party.',
+            'Smoked Trout Empanadas' => 'Every empanada is filled, made, and cooked by hand.',
+            'Mini Lamb Burger' => 'Although the flavors are slightly unusual, guests always go for seconds.',
+            'Duck Confit Quesadillas' => 'Sophistication and flavor come together in this perfectly cheesy quesadilla.'
+        ]; 
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_name' => 'Food App',
+            'meals' => $meals
         ]);
+    }
+
+    #[Route('/contact', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('main/contact.html.twig', []);
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('main/contact.html.twig', []);
     }
 }
