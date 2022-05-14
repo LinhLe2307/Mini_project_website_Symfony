aa0a93fb116b298f2d8d31daf18a272142450381
LinhLe2307 <lemylinh23071997@gmail.com>
2022-05-14 21:12:40 +0300

    Add homepage

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require("@symfony/webpack-encore");
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
 Encore
   // directory where compiled assets will be stored
-  .setOutputPath('public/build/')
+  .setOutputPath("public/build/")
   // public path used by the web server to access the output path
-  .setPublicPath('/build')
+  .setPublicPath("/build")
   // only needed for CDN's or sub-directory deploy
   //.setManifestKeyPrefix('build/')
 
@@ -20,10 +20,10 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
-  .addEntry('app', './assets/app.js')
+  .addEntry("app", "./assets/app.js")
 
   // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-  .enableStimulusBridge('./assets/controllers.json')
+  .enableStimulusBridge("./assets/controllers.json")
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
@@ -46,17 +46,17 @@ Encore
   .enableVersioning(Encore.isProduction())
 
   .configureBabel((config) => {
-    config.plugins.push('@babel/plugin-proposal-class-properties');
+    config.plugins.push("@babel/plugin-proposal-class-properties");
   })
 
   // enables @babel/preset-env polyfills
   .configureBabelPresetEnv((config) => {
-    config.useBuiltIns = 'usage';
+    config.useBuiltIns = "usage";
     config.corejs = 3;
   })
 
   // enables Sass/SCSS support
-  //.enableSassLoader()
+  .enableSassLoader()
 
   // uncomment if you use TypeScript
   //.enableTypeScriptLoader()
